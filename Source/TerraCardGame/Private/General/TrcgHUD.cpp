467843645311fff8c0f202d1ad2991c3ab7c25b1
Watz <watztheengineer@gmail.com>
2025-07-07 19:27:44 +0200

    refactor architecture to use HUD

---
+++ Source/TerraCardGame/Private/General/TrcgHUD.cpp
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "General/TrcgHUD.h"
+
+#include "General/TrcgGameInstance.h"
+#include "General/TrcgUserWidget.h"
+#include "Kismet/GameplayStatics.h"
+
+UTrcgGameInstance* ATrcgHUD::GetTrcgGameInstance() const
+{
+	UTrcgGameInstance* GI = Cast<UTrcgGameInstance>(UGameplayStatics::GetGameInstance(GetWorld()));
+	check(GI);
+	return GI;
+}
+
+UTrcgUserWidget* ATrcgHUD::GetWidgetFromClass(const TSubclassOf<UTrcgUserWidget> Class)
+{
+	for (UTrcgUserWidget* Widget : ActiveWidgets)
+	{
+		if (Widget->GetClass() == Class) return Widget;
+	}
+	return nullptr;
+}
---
+++ Source/TerraCardGame/Public/General/TrcgHUD.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "TrcgHUD.generated.h"
+
+class UTrcgUserWidget;
+class UTrcgGameInstance;
+/**
+ * 
+ */
+UCLASS()
+class TERRACARDGAME_API ATrcgHUD : public AHUD
+{
+	GENERATED_BODY()
+
+	UFUNCTION(BlueprintPure, meta=(CompactNodeTitle = "TRCG Instance", Category="TRCG"))
+	UTrcgGameInstance* GetTrcgGameInstance() const;
+
+	UFUNCTION(BlueprintCallable, Category = "TRCG")
+	UTrcgUserWidget* GetWidgetFromClass(const TSubclassOf<UTrcgUserWidget> Class);
+
+public:
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "TRCG")
+	TArray<UTrcgUserWidget*> ActiveWidgets;
+};
